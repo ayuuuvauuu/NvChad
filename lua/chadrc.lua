15e610256e381ab4050a22ca9bf0b9738f4f2409
ayush mishra <mail81mitthu@gmail.com>
2025-06-09 18:45:49 +0530

    m: update rc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,19 +1,19 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "tokyodark",
+  theme = "tokyodark",
   transparency = false,
   lsp_semantic_tokens = true, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
   theme_toggle = { "tokyodark", "gruvbox_light" },
 }
 
@@ -21,8 +21,8 @@ M.ui = {
   cmp = {
     icons = false,
     lspkind_text = true,
-    style = "default",         -- default/flat_light/flat_dark/atom/atom_colored
-    border_color = "grey_fg",    -- only applicable for "default" style, use color names from base30 variables
+    style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
     selected_item_bg = "colored", -- colored / simple
   },
 
@@ -32,16 +32,9 @@ M.ui = {
     -- round and block will work for minimal theme only
     separator_style = "default",
   },
-  lsp = {
-    -- show function signatures i.e args as you type
-    signature = {
-      disabled = false,
-      silent = true, -- silences 'no signature help available' message from appearing
-    },
-  },
+  lsp = { signature = true},
 }
 
 M.nvdash = { load_on_startup = true }
 
 return M
-
