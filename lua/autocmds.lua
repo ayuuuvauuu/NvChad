eb1be726f07f5059830033f2eac99ed82f0166e5
ayush mishra <99461867+ayuuuvauuu@users.noreply.github.com>
2026-04-29 12:57:36 +0530

    m: disbale lsp on startup of a buffer,
    to re enable it :lsp enable

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,15 +1,15 @@
 require "nvchad.autocmds"
 
--- vim.api.nvim_create_autocmd("User", {
---   pattern = "LspLoaded",
---   callback = function()
---     -- Check if any LSP clients are running
---     local clients = vim.lsp.get_clients()
---
---     -- Add a slight delay to ensure LSP is fully initialized
---     vim.defer_fn(function()
---       vim.cmd "LspStop"
---       print "LspStop executed"
---     end, 100)
---   end,
--- })
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LspLoaded",
+  callback = function()
+    -- Check if any LSP clients are running
+    local clients = vim.lsp.get_clients()
+
+    -- Add a slight delay to ensure LSP is fully initialized
+    vim.defer_fn(function()
+      vim.cmd "lsp disable"
+      print "LspStop executed"
+    end, 100)
+  end,
+})
